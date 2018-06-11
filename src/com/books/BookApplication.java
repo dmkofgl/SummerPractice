fe002272254ac4d5904150b0925ba0ae4800180a
Uladzislau_Barkou <dmkofgl@gmail.com>
2018-06-11 15:23:12 +0300

    added maven

--- src/com/books/BookApplication.java
+++ src/com/books/BookApplication.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         books = new ArrayList<Book>();
-        BookFilter filter = new BookFilter();
+        BookService filter = new BookService();
         Date date = new Date();
         Person[] authors = {
                 new Person("Ivan", "First"),
