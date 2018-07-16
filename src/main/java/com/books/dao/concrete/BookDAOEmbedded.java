1d584910a2fbe03fedc35e183eb7e348b42b181d
Uladzislau_Barkou <dmkofgl@gmail.com>
2018-07-16 10:54:02 +0300

    remove main class remove jar plugin maven

--- src/main/java/com/books/dao/concrete/BookDAOEmbedded.java
+++ src/main/java/com/books/dao/concrete/BookDAOEmbedded.java
@@ -1,6 +1,5 @@
 package com.books.dao.concrete;
 
-import com.books.BookApplication;
 import com.books.entities.Book;
 import com.books.entities.Person;
 import com.books.entities.Publisher;
@@ -15,7 +14,7 @@ import java.util.List;
 public class BookDAOEmbedded implements BookDAO {
 
     private static List<Book> books;
-    private static final Logger logger = LoggerFactory.getLogger(BookApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(BookDAOEmbedded.class);
 
     public BookDAOEmbedded(List<Book> books) {
         this.books = books;
